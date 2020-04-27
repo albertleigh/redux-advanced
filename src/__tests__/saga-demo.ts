e1265f7f24b1d17518a006efc9866017ef48f3e5
Wentao LI <liwentao90@yahoo.com>
2020-04-27 10:56:19 +0800

    [demo] add saga demo test cases trying to explain something 2#

--- src/__tests__/saga-demo.ts
+++ src/__tests__/saga-demo.ts
@@ -51,6 +51,117 @@ describe("saga api demo purpose test cases", ()=>{
 
   })
 
+  // benefits of using saga effect: saga effects can be regarded as the
+  // combination of thunk effect and rx-observable, with saga effect it no
+  // longer need to code in two separated groups to achieve one certain feature.
+  // additionally, comparing thunk w/ observable, saga requires less codes to
+  // achieve a equivalent functionally, in a nutshell,
+  // the less codes the less troubles
+  it("complex example", async()=> {
+    // before we dig in there are two reserved pattern utilized by saga groups
+    // 1) any saga "_$" in the path chain will be regarded as self handled saga
+    //  middleware won't intercept it by default
+    // 2) any saga "$$" in the path chain will be regarded as onInit handled saga
+    //  middleware will execute the saga once the module registered
+    // 3) by default the saga of the rest path pattern would be 'takeEvery'ed
+    // in another word, its corresponding saga will be executed for every single
+    // time the action dispatched
+
+    // the first input parameter of the saga will be the action object intercepted
+    // the `context` field will be the same context needed by the thunk effect
+
+    // each of the action impl of the `actions` handler of the effect context
+    // will bear another method  saga(payload):Generator, through which, the
+    // corresponding saga effect will be returned, and it input parameter will
+    // be the type of payload instead of the whole action, redux-advance will
+    // help you handle the creation of the action
+    // for the thunk effect, a on-the-fly created generator with an call effect
+    // of that promise will be created and returned for compatibility
+
+    const basicModel = defaultModelBuilder
+      .state(() => ({
+        name: "",
+        age: 0,
+      }))
+      .reducers({
+        setName(state, payload: string) {
+          state.name = payload;
+        },
+        setAge(state, payload: number) {
+          state.age = payload;
+        },
+      })
+      .sagas({
+        changeAll: function*(action) {
+          const { actions } = action.context;
+          yield put(actions.setName.create("basicName"));
+          yield put(actions.setAge.create(1));
+        },
+        _$customChange: function*(action) {
+          const { actions, getState } = action.context;
+          yield put(actions.setName.create(getState().name + "_custom"));
+          yield put(actions.setAge.create(getState().age + 1));
+        },
+        _$tkeLatestChange: function*(action) {
+          const { actions, getState } = action.context;
+          yield put(actions.setName.create(getState().name + "_latest"));
+          yield put(actions.setAge.create(getState().age + 2));
+        },
+      })
+      .sagas({
+        $$rootRoot: function*(action) {
+          const { actions } = action.context;
+          yield takeLatest(
+            actions._$tkeLatestChange.type,
+            actions._$tkeLatestChange.saga
+          );
+          const custemAct = yield take(actions._$customChange.type);
+          yield* actions._$customChange.saga(custemAct.payload);
+        },
+      })
+      .build();
+
+    const dependencies: Dependencies = { appName: "complex" };
+
+    const { getContainer, registerModels, gc } = init({
+      dependencies,
+      enableSaga: true,
+    });
+
+    registerModels({ basicModel });
+
+    const basicCtn = getContainer(basicModel);
+
+    expect(basicCtn.getState().age).toBe(0);
+    expect(basicCtn.getState().name).toBe("");
+
+    await basicCtn.actions.changeAll.dispatch({});
+
+    expect(basicCtn.getState().age).toBe(1);
+    expect(basicCtn.getState().name).toBe("basicName");
+
+    await basicCtn.actions._$customChange.dispatch({});
+
+    expect(basicCtn.getState().age).toBe(2);
+    expect(basicCtn.getState().name).toBe("basicName_custom");
+
+    await basicCtn.actions._$customChange.dispatch({});
+    // won't change this time
+    expect(basicCtn.getState().age).toBe(2);
+    expect(basicCtn.getState().name).toBe("basicName_custom");
+
+    await basicCtn.actions._$tkeLatestChange.dispatch({});
+    expect(basicCtn.getState().age).toBe(4);
+    expect(basicCtn.getState().name).toBe("basicName_custom_latest");
+
+    await basicCtn.actions._$tkeLatestChange.dispatch({});
+    expect(basicCtn.getState().age).toBe(6);
+    expect(basicCtn.getState().name).toBe("basicName_custom_latest_latest");
+
+  })
+
+  // unregistered example
+
   // another key concern of redux-advanced is implying typing, new saga
   // effect group also keep those in minds, at least will bear all the
   // typing implying that thunk effect group has
